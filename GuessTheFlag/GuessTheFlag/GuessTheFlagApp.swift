1757b6911ac161492668fe4b9f7418476e9e3b8f
Hussein <aisackhussein@gmail.com>
2023-09-28 13:32:04 +0300

    Fixed guess the flag app and the github error

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Muktar Hussein on 28/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
